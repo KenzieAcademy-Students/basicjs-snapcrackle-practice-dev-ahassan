02d9f5354e43f10b46606c11e0b0941ebdc280d4
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-03-20 12:56:34 +0000

    deploy: c6a9b08955289f24896beb009bf925ee373dc97f

--- code.js
+++ code.js
@@ -2,3 +2,38 @@
 // and select "Open with Live Server"
 
 // Your Code Here.
+function snapCrackle (maxValue) {
+    let result = '';
+
+    for (let i = 1; i <= maxValue; i++) {
+        if (i % 2 !== 0 && i % 5 === 0) {
+         
+          result += 'SnapCrackle, ';
+        } else if (i % 2 !== 0) {
+         
+          result += 'Snap, ';
+        } else if (i % 5 === 0) {
+         
+          result += 'Crackle, ';
+        } else {
+         
+          result += i + ', ';
+        }
+      }
+    
+      
+      return result;
+    }
+    //console.log(snapCrackle(12));
+
+    function render(text, maxValue) {
+        
+        document.write("<h1>Snap Crackle!</h1>");
+        document.write(`<h3>Max Value: ${maxValue}</h3>`);
+        document.write(`<p>${text}</p>`);
+      }
+      
+      const highestNumber = prompt("What is the highest number?");
+
+const result = snapCrackle(highestNumber);
+render(result, highestNumber);
